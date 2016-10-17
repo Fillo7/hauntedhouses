63a5362b0f3507eb7f378f6d47366d3081288c67
Ondrej Oravčok <ondrej.oravcok@unicornsystems.eu>
2016-10-17 16:58:28 +0100

    initial commit

---
+++ src/main/java/cz/muni/fi/pa165/hountedhouses/entity/House.java
@@ -0,0 +1,30 @@
+package cz.muni.fi.pa165.hountedhouses.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Ondro on 17-Oct-16.
+ */
+@Entity
+@Table(name = "Users")
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String name;
+
+    public House(){
+    }
+
+    public House(Long id){
+        this.id = id;
+    }
+
+}
