85baab6126f62a4ff6b2fd6de032a30e37916c7e
Fillo <Fillo@Fillo-Desktop>
2016-10-23 17:51:03 +0200

    Cleanup project files

--- src/test/java/cz/muni/fi/pa165/hountedhouses/HauntedhousesApplicationTests.java
+++ src/test/java/cz/muni/fi/pa165/hountedhouses/HauntedhousesApplicationTests.java
@@ -2,11 +2,7 @@ package cz.muni.fi.pa165.hountedhouses;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class HauntedhousesApplicationTests {
 
 	@Test
