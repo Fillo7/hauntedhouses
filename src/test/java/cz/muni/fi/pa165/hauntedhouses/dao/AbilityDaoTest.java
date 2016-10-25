09975224dda17ba667b2ba3421930ab7438126aa
Fillo <Fillo@147.251.46.74>
2016-10-25 15:33:19 +0200

    * Work on AbilityDao unit tests

--- src/test/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDaoTest.java
@@ -1,33 +1,86 @@
 package cz.muni.fi.pa165.hauntedhouses.dao;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import org.junit.After;
-import org.junit.Before;
+import cz.muni.fi.pa165.hauntedhouses.PersistenceApplicationContext;
+import cz.muni.fi.pa165.hauntedhouses.entity.Ability;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.Assert;
 
 /**
  * @author Filip Petrovic (422334)
  */
-@RunWith(MockitoJUnitRunner.class)
-public class AbilityDaoTest {
-    @PersistenceUnit
-    private EntityManagerFactory entityManagerFactory;
+@ContextConfiguration(classes=PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class AbilityDaoTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private AbilityDao abilityDao;
+	
+    @PersistenceContext 
+    private EntityManager entityManager;
     
-    private AbilityDao abilityDao = new AbilityDaoImpl();
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    @Test
+    public void testCreate() {
+        Ability ability = new Ability();
+        ability.setName("Freeze brain");
+        ability.setDescription("Freezes brain.");
+        
+        abilityDao.create(ability);
+        Assert.assertEquals(abilityDao.findById(ability.getId()).getName(), "Freeze brain");
+        Assert.assertEquals(abilityDao.findById(ability.getId()).getDescription(), "Freezes brain.");
     }
     
     @Test(expected = IllegalArgumentException.class)
-    public void testCreateNullAbility() {
-        abilityDao.create(null);
+    public void testCreateNull() {
+        Ability ability = null;
+        abilityDao.create(ability);		
+    }
+    
+    @Test
+    public void testFindAll() {
+        Ability first = new Ability();
+        Ability second = new Ability();
+	
+        first.setName("Freeze brain");
+        first.setDescription("Freezes brain.");
+	second.setName("Ignite hands");
+        second.setDescription("Ignites hands.");
+		
+        abilityDao.create(first);
+	abilityDao.create(second);
+
+        List<Ability> abilities = abilityDao.findAll();
+		
+        Assert.assertEquals(abilities.size(), 2);
+        
+        Ability assertFirst = new Ability();
+        Ability assertSecond = new Ability();
+        assertFirst.setName("Freeze brain");
+        assertFirst.setDescription("Freezes brain.");
+        assertSecond.setName("Ignite hands");
+        assertSecond.setDescription("Ignites hands.");
+		
+        Assert.assertTrue(abilities.contains(assertFirst));
+        Assert.assertTrue(abilities.contains(assertSecond));	
+    }
+	
+    @Test()
+    public void delete() {
+        Ability ability = new Ability();
+        ability.setName("Freeze brain");
+        ability.setDescription("Freezes brain.");
+        
+        abilityDao.create(ability);
+        Assert.assertNotNull(abilityDao.findById(ability.getId()));
+        abilityDao.delete(ability);
+        Assert.assertNull(abilityDao.findById(ability.getId()));
     }
 }
