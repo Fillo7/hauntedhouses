55ad2982ed737067b82ed7664fba057a7b7974c1
Tilwaen <leknerova@gmail.com>
2016-11-08 16:09:39 +0100

    API module, AbilityDTO stub
    Added Facade module folder hierarchy

---
+++ api-layer/src/main/java/cz/muni/fi/pa165/api/dto/AbilityDTO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.api.dto;
+
+/**
+ *
+ * @author Kristyna Loukotova
+ * @version 08.11.2016
+ */
+public class AbilityDTO {
+    
+}
