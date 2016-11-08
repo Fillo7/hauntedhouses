10d2f677cc0665efcc1be1e4531c85c99bbec399
Ondrej Oravčok <ondrej.oravcok@unicornsystems.eu>
2016-11-08 13:39:13 +0100

    [Ondro] divided project into maven modules

---
+++ persistence-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDaoTest.java
@@ -0,0 +1,167 @@
+package cz.muni.fi.pa165.hauntedhouses.dao;
+
+import cz.muni.fi.pa165.hauntedhouses.PersistenceApplicationContext;
+import cz.muni.fi.pa165.hauntedhouses.entity.Ability;
+import cz.muni.fi.pa165.hauntedhouses.entity.Monster;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.ValidationException;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * @author Filip Petrovic (422334)
+ */
+@ContextConfiguration(classes=PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class AbilityDaoTest extends AbstractTestNGSpringContextTests {
+    @Inject
+    private AbilityDao abilityDao;
+    
+    @PersistenceContext 
+    private EntityManager entityManager;
+    
+    private Ability abilityOne;
+    private Ability abilityTwo;
+    private Ability abilityThree;
+    
+    private Monster monster;
+    
+    @BeforeMethod
+    public void setAbilities() {
+        monster = new Monster();
+        monster.setName("Unicorn");
+        monster.setDescription("Leaves rainbows in its wake.");
+        
+        abilityOne = new Ability();
+        abilityOne.setName("Impale");
+        abilityOne.setDescription("Impales an enemy with its glorious horn.");
+
+        abilityTwo = new Ability();
+        abilityTwo.setName("Spawn rainbows");
+        abilityTwo.setDescription("Spawns rainbows all around the place, blinding anyone who dares to wander nearby.");
+        
+        abilityThree = new Ability();
+        abilityThree.setName("Charm");
+        abilityThree.setDescription("Charms an enemy with its extreme beauty making them do its bidding.");
+        
+        abilityOne.addMonster(monster);
+        abilityTwo.addMonster(monster);
+        abilityThree.addMonster(monster);
+        
+        monster.addAbility(abilityOne);
+        monster.addAbility(abilityTwo);
+        monster.addAbility(abilityThree);
+    }
+    
+    @Test
+    public void testCreate() {
+        abilityDao.create(abilityOne);
+        
+        Assert.assertEquals(abilityDao.findById(abilityOne.getId()).getName(), abilityOne.getName());
+        Assert.assertEquals(abilityDao.findById(abilityOne.getId()).getDescription(), abilityOne.getDescription());
+        Assert.assertEquals(abilityDao.findById(abilityOne.getId()).getMonsters(), abilityOne.getMonsters());
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateNullAbility() {
+        Ability ability = null;
+        abilityDao.create(ability);		
+    }
+    
+    @Test(expectedExceptions = ValidationException.class)
+    public void testCreateNullNameAbility() {
+        abilityOne.setName(null);
+        abilityDao.create(abilityOne);		
+    }
+    
+    @Test
+    public void testUpdate() {
+        abilityDao.create(abilityOne);
+        abilityDao.create(abilityThree);
+        
+        abilityThree.setName("Improved charm");
+        abilityDao.update(abilityThree);
+        
+        Ability found = abilityDao.findById(abilityThree.getId());
+        Assert.assertEquals(found.getName(), abilityThree.getName());	
+    }
+    
+    @Test
+    public void testDelete() {
+        abilityDao.create(abilityOne);
+        abilityDao.create(abilityThree);
+        
+        Assert.assertNotNull(abilityDao.findById(abilityOne.getId()));
+        abilityDao.delete(abilityOne);
+        Assert.assertNull(abilityDao.findById(abilityOne.getId()));
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDeleteNonExisting() {
+        abilityDao.delete(abilityOne);
+    }
+    
+    @Test
+    public void testFindById() {
+        abilityDao.create(abilityOne);
+        abilityDao.create(abilityTwo);
+        
+        Ability found = abilityDao.findById(abilityOne.getId());
+        Assert.assertEquals(found.getName(), abilityOne.getName());
+        Assert.assertEquals(found.getDescription(), abilityOne.getDescription());
+        Assert.assertEquals(found.getMonsters(), abilityOne.getMonsters());
+    }
+    
+    @Test
+    public void testFindByName() {
+        abilityDao.create(abilityOne);
+        abilityDao.create(abilityThree);
+        
+        Ability found = abilityDao.findByName(abilityOne.getName());
+        Assert.assertEquals(found.getName(), abilityOne.getName());
+        Assert.assertEquals(found.getDescription(), abilityOne.getDescription());
+        Assert.assertEquals(found.getMonsters(), abilityOne.getMonsters());
+    }
+    
+    @Test
+    public void testFindAll() {	
+        abilityDao.create(abilityOne);
+	abilityDao.create(abilityTwo);
+        abilityDao.create(abilityThree);
+
+        List<Ability> abilities = abilityDao.findAll();
+		
+        Assert.assertEquals(abilities.size(), 3);
+        
+        Ability assertFirst = new Ability();
+        Ability assertSecond = new Ability();
+        Ability assertThird = new Ability();
+        
+        assertFirst.setName("Impale");
+        assertFirst.setDescription("Impales an enemy with its glorious horn.");
+        assertFirst.addMonster(monster);
+        
+        assertSecond.setName("Spawn rainbows");
+        assertSecond.setDescription("Spawns rainbows all around the place, blinding anyone who dares to wander nearby.");
+        assertSecond.addMonster(monster);
+        
+        assertThird.setName("Charm");
+        assertThird.setDescription("Charms an enemy with its extreme beauty making them do its bidding.");
+        assertThird.addMonster(monster);
+		
+        Assert.assertTrue(abilities.contains(assertFirst));
+        Assert.assertTrue(abilities.contains(assertSecond));
+        Assert.assertTrue(abilities.contains(assertThird));	
+    }
+}
---
+++ persistence-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/dao/CursedObjectDaoTest.java
@@ -0,0 +1,248 @@
+package cz.muni.fi.pa165.hauntedhouses.dao;
+
+import cz.muni.fi.pa165.hauntedhouses.PersistenceApplicationContext;
+import cz.muni.fi.pa165.hauntedhouses.entity.CursedObject;
+import cz.muni.fi.pa165.hauntedhouses.entity.House;
+import cz.muni.fi.pa165.hauntedhouses.enums.MonsterAttractionFactor;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import static org.junit.Assert.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Marek Janco
+ */
+@ContextConfiguration(classes = PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class CursedObjectDaoTest extends AbstractTestNGSpringContextTests{
+    @Inject
+    private CursedObjectDao coDao;
+    
+    @Inject
+    private HouseDao houseDao;
+    
+    @PersistenceContext 
+    private EntityManager em;
+    
+    private CursedObject c1;
+    private CursedObject c2;
+    
+    private House h1;
+    private House h2;
+    
+    @BeforeMethod
+    public void before(){
+        h1 = new House();
+        h1.setName("house1");
+        h1.setAddress("dubnica");
+        houseDao.create(h1);
+        
+        h2 = new House();
+        h2.setName("house2");
+        h2.setAddress("brno");
+        houseDao.create(h2);
+        
+        c1 = new CursedObject();
+        c1.setName("doll");
+        c1.setDescription("");
+        c1.setHouse(h1);
+        c1.setMonsterAttractionFactor(MonsterAttractionFactor.LOW);
+        
+        c2 = new CursedObject();
+        c2.setName("necklace");
+        c2.setDescription("...");
+        c2.setHouse(h2);
+        c2.setMonsterAttractionFactor(MonsterAttractionFactor.MEDIUM);
+    }
+    
+    @Test
+    public void testCreate(){
+        coDao.create(c1);
+        
+        assertNotNull(c1.getId());
+        assertEquals(coDao.findById(c1.getId()).getName(), c1.getName());
+        assertEquals(coDao.findById(c1.getId()).getDescription(), c1.getDescription());
+        assertEquals(coDao.findById(c1.getId()).getHouse(), c1.getHouse());
+        assertEquals(coDao.findById(c1.getId()).getMonsterAttractionFactor(),
+                c1.getMonsterAttractionFactor());
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateNullCursedObject(){
+        coDao.create(null);
+    }
+    
+    @Test(expectedExceptions = ValidationException.class)
+    public void testCreateCursedObjectWithNullName(){
+        c1.setName(null);
+        coDao.create(c1);
+    }
+    
+    @Test(expectedExceptions = ValidationException.class)
+    public void testCreateCursedObjectWithNullDescription(){
+        c1.setDescription(null);
+        coDao.create(c1);
+    }
+
+    @Test(expectedExceptions = ValidationException.class)
+    public void testCreateCursedObjectWithNullHouse(){
+        c1.setHouse(null);
+        coDao.create(c1);
+    }
+    
+    @Test(expectedExceptions = ValidationException.class)
+    public void testCreateCursedObjectWithNullMonsterAttractionFactor(){
+        c1.setMonsterAttractionFactor(null);
+        coDao.create(c1);
+    }
+    
+    @Test()
+    public void testUpdate(){
+        coDao.create(c1);
+        c1.setName("new name");
+        c1.setDescription("new description");
+        c1.setHouse(h2);
+        c1.setMonsterAttractionFactor(MonsterAttractionFactor.INSANE);
+        
+        CursedObject upd =coDao.update(c1);
+        validate(upd, c1);
+        
+        c1.setName("old name");
+        upd =coDao.update(c1);
+        validate(upd, c1);
+        
+    }
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testUpdateNameToNull(){
+        coDao.create(c1);
+        
+        c1.setName(null);
+        CursedObject upd = coDao.update(c1);
+        em.flush();
+    }
+    
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testUpdateDescriptionToNull(){
+        coDao.create(c1);
+        
+        c1.setName(null);
+        CursedObject upd = coDao.update(c1);
+        em.flush();
+    }
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testUpdateHouseToNull(){
+        coDao.create(c1);
+        
+        c1.setHouse(null);
+        CursedObject upd = coDao.update(c1);
+        em.flush();
+    }
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testUpdateMonsterAttractionFactorToNull(){
+        coDao.create(c1);
+        
+        c1.setMonsterAttractionFactor(null);
+        CursedObject upd = coDao.update(c1);
+        em.flush();
+    }
+   
+    @Test(expectedExceptions = PersistenceException.class)
+    public void testUpdateWithSameName(){
+        coDao.create(c1);
+        coDao.create(c2);
+        
+        c1.setName("NAME");
+        c2.setName("NAME");
+        
+        coDao.update(c1);
+        em.flush();
+        coDao.update(c2);
+        em.flush();
+    }
+   
+    @Test()
+    public void testGetCursedObject(){
+        coDao.create(c1);
+        coDao.create(c2);
+        
+        CursedObject co = coDao.findById(c1.getId());
+        validate(co, c1);
+        co = coDao.findById(c2.getId());
+        validate(co, c2);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testGetNoExistingCursedObject(){
+        assertFalse(coDao.findAll().contains(c1));
+        CursedObject co = coDao.findById(c1.getId());
+    }
+    
+    @Test()
+    public void testGetAll(){
+        assertEquals(coDao.findAll().size(), 0);
+        coDao.create(c1);
+        assertEquals(coDao.findAll().size(), 1);
+        coDao.create(c2);
+        
+        assertEquals(coDao.findAll().size(), 2);
+        validate(coDao.findAll().get(0),c1);
+        validate(coDao.findAll().get(1),c2);
+    }
+    
+    @Test()
+    public void testDeleteCursedObject(){
+        coDao.create(c1);
+        coDao.create(c2);
+        assertEquals(coDao.findAll().size(), 2);
+        
+        coDao.delete(c1);
+        assertEquals(coDao.findAll().size(), 1);
+        validate(coDao.findAll().get(0),c2);
+        
+        coDao.delete(c2);
+        assertEquals(coDao.findAll().size(), 0);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDeleteNull(){
+        coDao.delete(null);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDeleteNotCreatedCursedObject(){
+        coDao.delete(c1);
+    }
+    
+    @Test(expectedExceptions = PersistenceException.class)
+    public void testCreateSameNamedCursedObjects(){
+        coDao.create(c1);
+        c2.setName(c1.getName());
+        coDao.create(c2);
+    }
+    
+    
+    private void validate(CursedObject upd, CursedObject c1){
+        assertEquals(upd, c1);
+        assertEquals(c1.getId(), upd.getId());
+        assertEquals(c1.getName(), upd.getName());
+        assertEquals(c1.getDescription(), upd.getDescription());
+        assertEquals(c1.getHouse(), upd.getHouse());
+    }
+    
+}
