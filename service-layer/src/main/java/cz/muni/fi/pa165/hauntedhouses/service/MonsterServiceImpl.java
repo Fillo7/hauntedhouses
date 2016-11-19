f475ff9c9a564bba8033feee6e3abad585fc82b0
marcus991 <marek.janco123@gmail.com>
2016-11-19 16:16:12 +0100

    monster service implementation

---
+++ service-layer/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/MonsterServiceImpl.java
@@ -0,0 +1,41 @@
+package cz.muni.fi.pa165.hauntedhouses.service;
+
+import cz.muni.fi.pa165.hauntedhouses.dao.MonsterDao;
+import cz.muni.fi.pa165.hauntedhouses.entity.Monster;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Marek Janco
+ */
+public class MonsterServiceImpl implements MonsterService{
+    @Inject
+    private MonsterDao monsterDao;
+    
+    @Override
+    public void create(Monster m) {
+        monsterDao.create(m);
+    }
+
+    @Override
+    public Monster update(Monster m) {
+        return monsterDao.update(m);
+    }
+
+    @Override
+    public void delete(Monster m) {
+        monsterDao.delete(m);
+    }
+
+    @Override
+    public Monster findById(Long id) {
+        return monsterDao.findById(id);
+    }
+
+    @Override
+    public List<Monster> findAll() {
+        return monsterDao.findAll();
+    }
+    
+}
