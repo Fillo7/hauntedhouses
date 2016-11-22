6ac83101a9c0a48f150e908eab283f73a9c5b907
Fillo <Fillo@147.251.209.37>
2016-11-22 19:50:29 +0100

    Added basic tests for AbilityService

--- service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/facade/AbilityFacadeTest.java
+++ service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/facade/AbilityFacadeTest.java
@@ -1,8 +1,16 @@
 package cz.muni.fi.pa165.hauntedhouses.facade;
 
+import cz.muni.fi.pa165.hauntedhouses.dto.AbilityCreateDTO;
+import cz.muni.fi.pa165.hauntedhouses.dto.AbilityDTO;
+import javax.inject.Inject;
+
 /**
  * @author Filip Petrovic (422334)
  */
 public class AbilityFacadeTest {
-    // To do
+    @Inject
+    AbilityFacade abilityFacade;
+    
+    private AbilityCreateDTO create;
+    private AbilityDTO ability;
 }
--- service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/service/AbilityServiceTest.java
+++ service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/service/AbilityServiceTest.java
@@ -4,15 +4,22 @@ import cz.muni.fi.pa165.hauntedhouses.ServiceConfiguration;
 import cz.muni.fi.pa165.hauntedhouses.dao.AbilityDao;
 import cz.muni.fi.pa165.hauntedhouses.entity.Ability;
 import cz.muni.fi.pa165.hauntedhouses.entity.Monster;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  * @author Filip Petrovic (422334)
@@ -37,19 +44,74 @@ public class AbilityServiceTest extends AbstractTestNGSpringContextTests {
         monster.setName("Doppelganger");
         monster.setDescription("Its a doppelganger.");
         monster.addAbility(one);
+        monster.addAbility(two);
         
         one = new Ability();
+        one.setId(0L);
         one.setName("Shameless copy");
         one.setDescription("Shamelessly copies what its target is doing.");
         one.addMonster(monster);
         
         two = new Ability();
+        two.setId(1L);
         two.setName("Charm");
         two.setDescription("Charms an enemy with its extreme beauty making them do its bidding.");
+        two.addMonster(monster);
     }
     
     @BeforeClass
     public void setup() throws ServiceException {
         MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void testCreate() {
+        abilityService.create(one);
+        
+        verify(abilityDao, times(1)).create(one);
+    }
+
+    @Test
+    public void testUpdate() {
+        when(abilityDao.update(one)).thenReturn(one);
+        
+        Ability updated = abilityService.update(one);
+        assertDeepEquals(updated, one);
+    }
+
+    @Test
+    public void testRemove() {
+        abilityService.remove(one);
+        
+        verify(abilityDao, times(1)).delete(one);
+    }
+
+    @Test
+    public void testFindById() {
+        when(abilityDao.findById(one.getId())).thenReturn(one);
+        
+        Ability retrieved = abilityService.findById(one.getId());
+        assertDeepEquals(retrieved, one);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Ability> list = new ArrayList<>();
+        list.add(one);
+        list.add(two);
+        
+        when(abilityDao.findAll()).thenReturn(list);
+        
+        List<Ability> retrieved = abilityService.findAll();
+        Assert.assertEquals(retrieved.size(), 2);
+        Assert.assertTrue(retrieved.get(0).getId().equals(0L));
+        Assert.assertTrue(retrieved.get(1).getId().equals(1L));
+    }
+    
+    private void assertDeepEquals(Ability actual, Ability expected) {
+        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual.getName(), expected.getName());
+        Assert.assertEquals(actual.getDescription(), expected.getDescription());
+        Assert.assertEquals(actual.getMonsters(), expected.getMonsters());
     }
 }
