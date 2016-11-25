3353ff23d132760ec07625fbec2b46671d0aa104
Fillo <Fillo@Fillo-Desktop>
2016-11-25 17:29:40 +0100

    Experimental changes to AbilityFacade tests

--- service-layer/src/main/java/cz/muni/fi/pa165/hauntedhouses/ServiceConfiguration.java
+++ service-layer/src/main/java/cz/muni/fi/pa165/hauntedhouses/ServiceConfiguration.java
@@ -1,19 +1,7 @@
 package cz.muni.fi.pa165.hauntedhouses;
 
-import cz.muni.fi.pa165.hauntedhouses.dto.AbilityDTO;
-import cz.muni.fi.pa165.hauntedhouses.dto.CursedObjectDTO;
-import cz.muni.fi.pa165.hauntedhouses.dto.HouseDTO;
-import cz.muni.fi.pa165.hauntedhouses.dto.MonsterDTO;
-import cz.muni.fi.pa165.hauntedhouses.entity.Ability;
-import cz.muni.fi.pa165.hauntedhouses.entity.CursedObject;
-import cz.muni.fi.pa165.hauntedhouses.entity.House;
-import cz.muni.fi.pa165.hauntedhouses.entity.Monster;
-import cz.muni.fi.pa165.hauntedhouses.exceptions.ServiceExceptionTranslateAspect;
-import cz.muni.fi.pa165.hauntedhouses.facade.MonsterFacadeImpl;
-import cz.muni.fi.pa165.hauntedhouses.service.MonsterServiceImpl;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
-import org.dozer.loader.api.BeanMappingBuilder;
 import org.springframework.context.annotation.*;
 
 import java.util.ArrayList;
@@ -27,23 +15,6 @@ import java.util.List;
 @Import(PersistenceApplicationContext.class)
 @ComponentScan(basePackages = "cz.muni.fi.pa165.hauntedhouses")
 public class ServiceConfiguration {
-
-    /*@Bean
-    public Mapper dozer() {
-        DozerBeanMapper beanMapper = new DozerBeanMapper();
-        beanMapper.addMapping(new DozerMappingConfiguration());
-        return beanMapper;
-    }
-
-    public class DozerMappingConfiguration extends BeanMappingBuilder {
-        @Override
-        protected void configure() {
-            mapping(CursedObject.class, CursedObjectDTO.class);
-            mapping(House.class, HouseDTO.class);
-            mapping(Ability.class, AbilityDTO.class);
-            mapping(Monster.class, MonsterDTO.class);
-        }
-    }*/
 
     @Bean
     public Mapper dozer() {
--- service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/facade/AbilityFacadeTest.java
+++ service-layer/src/test/java/cz/muni/fi/pa165/hauntedhouses/facade/AbilityFacadeTest.java
@@ -4,15 +4,18 @@ import cz.muni.fi.pa165.hauntedhouses.BeanMappingService;
 import cz.muni.fi.pa165.hauntedhouses.ServiceConfiguration;
 import cz.muni.fi.pa165.hauntedhouses.dto.AbilityCreateDTO;
 import cz.muni.fi.pa165.hauntedhouses.dto.AbilityDTO;
+import cz.muni.fi.pa165.hauntedhouses.entity.Ability;
 import cz.muni.fi.pa165.hauntedhouses.service.AbilityService;
 import java.util.List;
-import javax.inject.Inject;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -22,57 +25,73 @@ import org.testng.annotations.Test;
  * @author Filip Petrovic (422334)
  */
 @ContextConfiguration(classes=ServiceConfiguration.class)
-public class AbilityFacadeTest extends AbstractTransactionalTestNGSpringContextTests {
-    /*@Mock
+public class AbilityFacadeTest extends AbstractTestNGSpringContextTests {
+    @Mock
     AbilityService abilityService;
     
     @Mock
-    BeanMappingService beanMappingService;*/
+    BeanMappingService beanMappingService;
     
-    @Inject
-    //@InjectMocks
-    AbilityFacade abilityFacade;
+    @InjectMocks
+    AbilityFacade abilityFacade = new AbilityFacadeImpl();
 
     private AbilityCreateDTO createFirst;
     private AbilityCreateDTO createSecond;
     private AbilityDTO ability;
     
-    /*@BeforeClass
-    public void setUpClass() throws ServiceException {
-        MockitoAnnotations.initMocks(this);
-    }*/
+    private Ability first;
+    private Ability second;
+    private Ability third;
     
     @BeforeMethod
     public void prepareAbilities() {
         createFirst = new AbilityCreateDTO();
         createFirst.setName("Shameless copy");
         createFirst.setDescription("Shamelessly copies what its target is doing.");
-
+        
+        first = new Ability();
+        first.setName("Shameless copy");
+        first.setDescription("Shamelessly copies what its target is doing.");
+ 
         createSecond = new AbilityCreateDTO();
         createSecond.setName("Another shameless copy");
         createSecond.setDescription("Running out of ideas.");
-
+        
+        second = new Ability();
+        second.setName("Another shameless copy");
+        second.setDescription("Running out of ideas.");
+        
         ability = new AbilityDTO();
         ability.setName("Charm");
         ability.setDescription("Charms an enemy with its extreme beauty making them do its bidding.");
+        
+        third = new Ability();
+        third.setName("Charm");
+        third.setDescription("Charms an enemy with its extreme beauty making them do its bidding.");
     }
 
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+    
     @Test
     public void testCreate() {
-        Long id = abilityFacade.createAbility(createFirst);
-        assertDeepEquals(createFirst, abilityFacade.getAbilityById(id));
+        when(beanMappingService.mapTo(createFirst, Ability.class)).thenReturn(first);
+        
+        abilityFacade.createAbility(createFirst);
+        verify(abilityService, times(1)).create(first);
     }
 
     @Test
     public void testUpdate() {
-        Long id = abilityFacade.createAbility(createFirst);
-        ability.setId(id);
+        when(beanMappingService.mapTo(ability, Ability.class)).thenReturn(third);
+        
         abilityFacade.updateAbility(ability);
-
-        assertDeepEquals(ability, abilityFacade.getAbilityById(id));
+        verify(abilityService, times(1)).update(third);
     }
 
-    @Test
+    /*@Test
     public void testDelete() {
         Long id = abilityFacade.createAbility(createFirst);
         Assert.assertEquals(abilityFacade.getAllAbilities().size(), 1);
@@ -120,5 +139,5 @@ public class AbilityFacadeTest extends AbstractTransactionalTestNGSpringContextT
     private void assertDeepEquals(AbilityDTO actual, AbilityDTO expected) {
         Assert.assertEquals(actual.getName(), expected.getName());
         Assert.assertEquals(actual.getDescription(), expected.getDescription());
-    }
+    }*/
 }
