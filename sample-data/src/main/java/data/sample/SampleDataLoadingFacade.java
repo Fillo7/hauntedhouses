668557146dc5aef1e3c16016c9148ac3d3ee1959
Ondrej Oravčok <ondrej.oravcok@unicornsystems.eu>
2016-12-01 17:55:19 +0100

    [Ondro] sample-data init

---
+++ sample-data/src/main/java/data/sample/SampleDataLoadingFacade.java
@@ -0,0 +1,15 @@
+package data.sample;
+
+/**
+ * facade for data loading
+ *
+ * Created by Ondrej Oravcok on 01-Dec-16.
+ */
+public interface SampleDataLoadingFacade {
+
+    /**
+     * loads data if sample data needed
+     */
+    void loadData();
+
+}
---
+++ sample-data/src/main/java/data/sample/SampleDataLoadingFacadeImpl.java
@@ -0,0 +1,54 @@
+package data.sample;
+
+import cz.muni.fi.pa165.hauntedhouses.entity.Monster;
+import cz.muni.fi.pa165.hauntedhouses.service.MonsterService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * implementation of sample data loading facade SampleLoadingFacade
+ *
+ * Created by Ondrej Oravcok on 01-Dec-16.
+ */
+@Transactional
+@Component
+public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
+
+    private final Map<String, Monster> monsters = new HashMap<>();
+
+    @Inject
+    private MonsterService monsterService;
+
+    @Override
+    public void loadData() {
+        generateMonsters();
+        generateHouses();
+    }
+
+    private void generateMonsters(){
+        monsters.put("kitty", createMonster("Little Kitty", "little sweet kitty", LocalTime.of(10, 20), LocalTime.of(13, 40)));
+        monsters.put("huggy", createMonster("Huggy Bear", "Snoop Dogg Huggy Bear", LocalTime.of(4, 0), LocalTime.of(4, 5)));
+    }
+
+    private void generateHouses(){
+
+    }
+
+    private Monster createMonster(String name, String description, LocalTime hauntedStart, LocalTime hauntedEnd){
+        Monster monster = new Monster();
+
+        monster.setName(name);
+        monster.setDescription(description);
+        monster.setHauntedIntervalStart(hauntedStart);
+        monster.setHauntedIntervalEnd(hauntedEnd);
+
+        monsterService.create(monster);
+        return monster;
+    }
+
+}
