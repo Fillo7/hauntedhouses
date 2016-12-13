9e52011eaf6a98423f723d14ec494246e7c6f183
xjanco1 <xjanco1@nymfe83.fi.muni.cz>
2016-12-13 13:46:16 +0100

    cursed object uri added

--- rest-api-layer/src/main/java/cz/muni/fi/pa165/hauntedhouses/configuration/Uri.java
+++ rest-api-layer/src/main/java/cz/muni/fi/pa165/hauntedhouses/configuration/Uri.java
@@ -8,4 +8,5 @@ public class Uri {
     public static final String MONSTERS = "/monsters";
     public static final String HOUSES = "/houses";
     public static final String ABILITIES = "/abilities";
+    public static final String CURSED_OBJECTS = "/cursedObjects";
 }
