9408053419aefb2202fc6176c8ddb696442123d4
Filip Petrovic <mail.filip.petrovic@gmail.com>
2016-12-14 22:18:16 +0100

    Restored original Angular front-end (working), bower is not needed anymore

--- front-end-and-rest-layer/src/main/webapp/scripts/controllers/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.driversList = 'test 234';
-  });
--- front-end-and-rest-layer/src/main/webapp/scripts/controllers/monsters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('angularApp')
-    .controller('MonstersCtrl', function ($scope, $http) {
-        $http.get('/pa165/rest/monsters').then(function (response) {
-            $scope.priserky = response.data;
-        });
-    });
